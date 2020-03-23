8a37bcc46698299bde7f0c61c200f3a5273cfbbf
Inka Kekäläinen <inka.kekalainen@gmail.com>
2020-03-23 11:01:29 +0100

    eka commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -21,6 +22,12 @@ class App extends React.Component {
   render() {
     return (
         <div className="App">
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+                integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+                crossOrigin="anonymous"
+            />
 
           <Header/>
           <Main/>
