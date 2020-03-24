ba637548a7e1bdd9f5fb679207b34b6eb179a10f
lejuuso <juuso.leppanen@helsinki.fi>
2020-03-24 16:00:02 +0200

    delete

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,9 +1,35 @@
-import React from "react"
+import React, {useState} from "react"
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 function Delete() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div className={"main"}>
-            <p>this is delete</p>
+        <div>
+            <Button variant="primary" onClick={handleShow}>
+                Poista ilmoitus
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Syötä sähköposti</Modal.Title>
+                </Modal.Header>
+                <Modal.Body><input/></Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Peruuta
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Poista
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
-    )
+    );
+
+
 }
 export default Delete
