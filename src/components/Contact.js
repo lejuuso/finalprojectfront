55d4e1f2e399bda1811a786e214e2357f0f9be78
lejuuso <juuso.leppanen@helsinki.fi>
2020-03-24 14:24:34 +0200

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,37 @@
-import React from "react"
+import React, {useState} from "react"
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
 function Contact() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div className={"main"}>
-            <p>this is contact</p>
-        </div>
-    )
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Vastaa ilmoitukseen
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Vastaus</Modal.Title>
+                </Modal.Header>
+                <Modal.Body><input type="text"/></Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Sulje
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Lähetä
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+
+
+
 }
 export default Contact
