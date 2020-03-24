ee914337c77f2013d1ff8063bc9288c7edbd9da8
saanaraw <saana.railo@gmail.com>
2020-03-24 14:45:10 +0200

    header ei enää kortti, jätä ilmoitus-modaali ilmestyy (post.js)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,12 @@
 import React from "react"
 import {Card, Button} from "react-bootstrap";
 
+
 function Header() {
     return (
         <div className={"header"}>
-            <Card>
-                <Card.Body>
-                    <Card.Title>Korona-apu</Card.Title>
-                    <Card.Text>
-                        Naapurit apunasi korona-aikana
-                    </Card.Text>
-                    <Button variant="primary">Jätä ilmoitus</Button>
-                </Card.Body>
-            </Card>
+            <h2>Korona-apu</h2>
+            <p> Naapurit apunasi korona-aikana</p>
         </div>
     )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,33 @@
-import React from "react"
+import React, {Component, useState} from "react"
+import {Modal, Button} from "react-bootstrap"
+
 function Post() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div className={"main"}>
-            <p>this is post</p>
-        </div>
-    )
+        <>
+            <Button className={"button"} variant="primary" onClick={handleShow}>
+                Jätä ilmoitus
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Jätä ilmoitus</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Jätä ilmoitus avun tarpeesta tai tarjoamisesta.</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Peruuta
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Lähetä
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 export default Post
